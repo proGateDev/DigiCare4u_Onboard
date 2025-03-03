1fa92dbcd131ecd74b9419e935dfc19e9b4419a2
Vikas Gupta <vikasgupta.sparx@gmail.com>
2025-03-03 11:23:16 +0530

    api endpoint changed

--- constants/endPoints.js
+++ constants/endPoints.js
@@ -1,4 +1,4 @@
 // export const  devURL= "https://digicare4u-chhqh0cubtehc6h6.centralindia-01.azurewebsites.net"
 // export const devURL = "http://192.168.1.109:8000";
-export const devURL = "https://digicare4u-backend.onrender.com";
+export const devURL = "https://render-be-a967.onrender.com";
 
